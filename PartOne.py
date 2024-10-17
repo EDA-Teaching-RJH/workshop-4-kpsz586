c6a66bc4444f0cdb8f59a5c6c158e9a093b47667
kpsz586 <kp586@kent.ac.uk>
2024-10-17 13:42:56 +0000

    Workshop 4

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,13 @@
-
+cIn = input("Enter the name of your variable in camel case: ")
+def change_case(str):
+    res = [cIn[0].lower()]
+    for c in cIn[1:]:
+        if c in ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+            res.append('_')
+            res.append(c.lower())
+        else:
+            res.append(c)
+     
+    return ''.join(res)
+     
+print(change_case(str))
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,22 @@
-
+print("Your coffe costs 75p. This machine accepts only 50p, 20p, 10p and 5p coins.")
+coin_inserted = 0
+price = int(75)
+while int(coin_inserted) < 75:
+    coin_inserted = int(input("Enter the inserted coin value: "))
+    if coin_inserted == 5:
+        price = price - coin_inserted
+        print("You still have to pay "+str(price))
+    elif coin_inserted == 10:
+        price = price - coin_inserted
+        print("You still have to pay "+str(price))
+    elif coin_inserted == 20:
+        price = price - coin_inserted
+        print("You still have to pay "+str(price))
+    elif coin_inserted == 50:
+        price = price - coin_inserted
+        print("You still have to pay "+str(price))
+    else:
+        print("The coin value is incorrect")
+    if price <= 0:
+        print("You've successfully purchased the coffee.")
+        break
